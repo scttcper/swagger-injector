0477f0d03d09184b680e2751197c320de544ba3a
Scott Cooper <scttcper@gmail.com>
2017-04-04 11:51:38 -0700

    add tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,15 +4,20 @@ const request = require('supertest');
 
 lint(['lib', 'test']);
 
+const json = require('../package.json');
+
 describe('koa2-swagger-ui', function() {
-  it('should return index file', function(done) {
-    request(app.listen())
+  it('should return index file', function() {
+    return request(app.listen())
       .get('/docs')
       .expect('Content-Type', /html/)
-      .expect(200)
-      .end((err) => {
-        if (err) return done(err);
-        return done();
-      });
+      .expect(200);
+  });
+  it('should return index file', function() {
+    const url = `https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/${json.devDependencies['swagger-ui']}`;
+    return request(url)
+      .get('/swagger-ui.css')
+      .expect('Content-Type', 'text/css')
+      .expect(200);
   });
 });
