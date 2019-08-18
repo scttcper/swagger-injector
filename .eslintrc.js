2fef4758eeaac91d2a81cba40b1feace05f4f5fb
Scott Cooper <scttcper@gmail.com>
2019-08-18 14:35:49 -0700

    fix: upgrade swagger

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['xo-space/esnext', 'xo-typescript', 'prettier/@typescript-eslint'],
+  extends: ['xo-space/esnext', 'xo-typescript'],
   rules: {
     'object-curly-spacing': ['error', 'always'],
     '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
     '@typescript-eslint/explicit-function-return-type': 0,
     'capitalized-comments': 0,
+    '@typescript-eslint/no-explicit-any': 0,
     'comma-dangle': ['error', 'always-multiline'],
   },
 };
